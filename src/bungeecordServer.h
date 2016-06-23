3c678812f9496379e154e2d44dc5e969e325c2cd
BuildTools <melmccalla@gmail.com>
2016-06-23 12:24:08 -0700

    Added files src/bungeecordServer.h and src/bungeecordServer.cpp from master

---
+++ src/bungeecordServer.h
@@ -0,0 +1,51 @@
+#ifndef BUNGEECORD_SERVER_H
+#define BUNGEECORD_SERVER_H
+#include "server.h"
+#include "ServerStream.h"
+#include "log4cpp/Category.hh"
+#include <string>
+#include <sstream>
+#include <vector>
+#include <thread>
+#include <time.h>
+namespace MinecraftServerService {
+class BungeeCordServer : public Server
+{
+	public:
+		BungeeCordServer(
+			std::string serverName,
+			std::string serverPath, std::string serverJarName, std::string serverAccount,
+			int maxHeapAlloc, int minHeapAlloc, int gcThreadCount,
+			std::string backupPath,
+			std::vector<std::string> javaArgs, 
+			std::vector<std::string> serverOptions
+		);
+		virtual ~BungeeCordServer();
+		void updateServer();
+		void backupServer();
+		void backupServer(std::string backupPath);
+		void serverStatus();
+		void startServer();
+		void stopServer();
+		void restartServer();
+		void reloadServer();
+		std::string listOnlinePlayers();
+		void listOnlinePlayers(std::string playerName);
+		void sendCommand(std::string command);
+		std::string serverName;
+	protected:
+		void logger(size_t linesRequested, std::stringstream* output, log4cpp::Category* log);
+		std::string serverPath;
+		std::string serverJarName;
+		std::string serverAccount;
+		int maxHeapAlloc;
+		int minHeapAlloc;
+		int gcThreadCount;
+		std::string backupPath;
+		std::vector<std::string> javaArgs;
+		std::vector<std::string> serverOptions;
+		std::iostream* serverProcess = nullptr;
+		log4cpp::Category* log = nullptr;
+};
+}
+#endif /* BUNGEECORD_SERVER_H */
